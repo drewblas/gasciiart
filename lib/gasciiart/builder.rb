27e8e666d35a7a5cbf3de31b6e5ad182542d274a
Drew Blas <drew.blas@gmail.com>
2012-05-07 10:01:00 -0500

    Builder creates the basic git repo

--- lib/gasciiart/builder.rb
+++ lib/gasciiart/builder.rb
@@ -1,12 +1,89 @@
+require 'fileutils'
+
 class Gasciiart::Builder
   attr_reader :source, :target, :options
 
   def initialize(source, target, options = {})
     @source, @target, @options = source, target, options
+    @source = "#{@source}/" unless @source[-1..-1] == '/'
+    @target = "#{@target}/" unless @target[-1..-1] == '/'
+    @verbose = options[:verbose]
+
+    @starting_dir = Dir.pwd
+    @target_animation = "#{@target}animation.txt"
   end
 
   # Where all the magic happens
+  # 
   def build!
+    test_git!
 
+    file_list = Dir.glob "#{@source}*" # Pull the file list before creating the target directory
+
+    setup_target
+
+    file_list.each do |infile_name|
+      rewrite_animation_frame infile_name
+      create_commit infile_name
+    end
+  end
+
+  def setup_target
+    log "Setting up #{@target}"
+
+    if File.exists? @target
+      raise ArgumentError, %{Target directory "#{@target}" already exists}
+    end
+
+    FileUtils.mkdir @target
+
+    run "git init ."
+
+    # TODO: Create runner file
+  end
+
+  def rewrite_animation_frame(infile_name)
+    log "Writing #{infile_name}"
+    File.open(infile_name) do |infile|
+      File.open(@target_animation, 'w') do |outfile|
+        outfile.write infile.read  # This could be streamed, but a single animation frame is small enough.  Let's start simple
+      end
+    end
+  end
+
+  def create_commit(infile_name)
+    run %{git add animation.txt && git commit -m "#{infile_name}"}
+  end
+
+  protected
+
+  # Log the message if verbose is on
+  def log(msg)
+    puts msg if @verbose
+  end
+
+  # Ensures there is the git command available
+  # TODO: Test git version
+  def test_git!
+    result = `which git`
+    if $?.exitstatus != 0
+      raise IOError, "Can't find git"
+    end
+  end
+
+  # Runs the specified command
+  #
+  # All run commands happen in the target dir
+  # but all file paths are relative to the starting directory, so all the switching happens here
+  def run(cmd)
+    Dir.chdir @target
+
+    log "Running: #{cmd}"
+    `#{cmd}`
+    if $?.exitstatus != 0
+      raise IOError, %{Error executing command: "#{cmd}"}
+    end
+
+    Dir.chdir @starting_dir
   end
 end
--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -1,17 +1,29 @@
 require 'spec_helper'
 
 describe Gasciiart::Builder do
+  let(:builder) { Gasciiart::Builder.new('./', 'output') }
+  subject { builder }
+
   it 'should initialize' do
-    builder = nil
+    b = nil
 
     # Not really necessary, since this exception isn't caught,
     # but that's all this spec does, so let's be explicit
     expect {
-      builder = Gasciiart::Builder.new('./', 'output')
+      b = Gasciiart::Builder.new('./', 'output')
     }.not_to raise_exception
 
-    builder.source.should  == './'
-    builder.target.should  == 'output'
-    builder.options.should == {}
+    b.source.should  == './'
+    b.target.should  == 'output/'
+    b.options.should == {}
   end
+
+  context "setup_target" do
+    it "should create the directory" do
+      builder.setup_target
+      File.directory?("output/").should == true
+    end
+  end
+
+
 end
