08b98ba0cfdbbab12944d0cbf0c582c01449bc5d
Drew Blas <drew.blas@gmail.com>
2012-05-07 12:41:18 -0500

    Output auto-sets BG color

--- lib/gasciiart.rb
+++ lib/gasciiart.rb
@@ -5,6 +5,8 @@ module Gasciiart
   RUNNER = <<-EOF
 #!/bin/bash
 
+echo "\033[0;40m"
+
 for ref in `git rev-list HEAD --reverse`
 do
   git checkout $ref > /dev/null 2> /dev/null
@@ -13,5 +15,6 @@ do
   sleep 0.05
 done
 
+git checkout HEAD
   EOF
 end
